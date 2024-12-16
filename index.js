62e58a38626717e3c4cd1e486327dbcb177af8e6
Zana <zana.niswah@gmail.com>
2024-12-16 13:59:57 +0700

    Senin, 16 Des Siang

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const cors = require("cors")
 const userRouter = require("./routers/users.router");
 
 const app = express();
 const port = process.env.APP_PORT;
-
+app.use(cors)
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
--- routers/users.router.js
+++ routers/users.router.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const authenticateToken = require("../middlewares/auth.middleware");
-// const userController = require("../controllers/users.controller");
+const authenticateToken = require("../middlewares/auth.middleware");
+const userController = require("../controllers/users.controller");
 const userRepository = require("../repositories/users.repository");
 const transactionRepository = require("../repositories/transactions.repository");
 // // const transactionsController = require("../controllers/transactions.controller");
 
 // router.get("/users", userRepository.getUsers);
-router.get("/transactions", transactionRepository.getTransactions);
-// router.post("/users", userController.createUser);
-// router.get("/users", userController.getUser);
-// router.get("/profile", authenticateToken, userController.getUserById);
-// router.post("/login", userController.login);
+// router.get("/transactions", transactionRepository.getTransactions);
+router.post("/users", userController.createUser);
+router.get("/profile", authenticateToken, userController.getUserById);
+router.post("/auth/login", userController.login);
 // router.get("/transactions", transactionsController.getTransactionsById);
+// router.get("/users", userController.getUser);
 module.exports = router;
